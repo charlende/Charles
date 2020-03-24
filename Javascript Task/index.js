040d714035baf8d9780ff5d446e809480ae34f8c
charlende <61964113+charlende@users.noreply.github.com>
2020-03-24 16:22:29 +0100

    Add files via upload
    Javascript Task

---
+++ Javascript Task/index.js
@@ -0,0 +1,15 @@
+
+    var my_full_name = "Charles Alende";
+    console.log(my_full_name);
+    
+    var my_courses = ['HMTL', 'CSS', 'Javascript'];
+    console.log(my_courses);
+
+    for(let i = 0; i < 200; i++) {
+        if (i % 2 !== 0) {
+            console.log(i)
+        }
+        
+        }
+    
+
