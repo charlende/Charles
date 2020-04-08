60d8e4b19e9093a33730e51df45dd0f2fd2a01cb
charlende <61964113+charlende@users.noreply.github.com>
2020-04-08 21:51:40 +0100

    Add files via upload
    JavaScript Task 2

---
+++ task2.js
@@ -0,0 +1,46 @@
+let data = [
+    {principal: 2500, time: 1.8},
+    {principal: 1000, time: 5},
+    {principal: 3000, time: 1},
+    {principal: 2000, time: 3}
+];
+
+  function interestCalculator(data){
+   interestData = []
+   for(let i = 0; i < data.length; i++){
+       if(data[i].principal >= 2500 && (data[i].time > 1 && data[i].time <3)){
+            data[i].rate = 3
+        }
+
+       else if (data[i].principal >= 2500 && data[i].time >= 3) {
+            data[i].rate = 4
+        }
+
+       else if (data[i].principal < 2500 && data[i].time <= 1) {
+            data[i].rate = 2
+        }
+
+       else data[i].rate = 1;
+
+        interestData.push(data[i]);
+    }
+       console.log(interestData);
+       return interestData;
+    } 
+
+      interestCalculator(data);
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
